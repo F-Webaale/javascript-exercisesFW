cf70f215f2a5f8b10de2820467e84343e6928c69
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-03-06 23:00:03 +0300

    Add solution to js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string="";
+    for (i=0 ; i<num; i++) {
+         string=word+string;   };
+    return string
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
