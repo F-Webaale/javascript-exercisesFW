c955279cc659baf110fd9da87ef00759b00730eb
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-03-07 09:06:13 +0300

    Generate a random integer a couple of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,17 @@
 const repeatString = function(word, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+
     let string="";
+
     for (i=0 ; i<num; i++) {
          string=word+string;   };
     return string
+
+    let repeat = function(num) {
+        num = math.floor(math.Random() * 1000);
+    }
 };
 
 
