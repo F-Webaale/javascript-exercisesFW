1b707094cfb6c63449d961f825ffbe2e3590c507
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-03-24 09:09:01 +0300

    Add solutions to js exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+function removeFromArray(arr, ...num) {
+  const newArray = [];
+  arr.forEach(myFunction) 
+      function myFunction(value, index, array) {
+          if (!num.includes(value)){
+          newArray.push(value);
+      }}
+  return newArray;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
