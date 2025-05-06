5a413997c8d94bdb64ccb9c899d67fbde533eb63
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-05-06 20:19:39 +0300

    Added a solution that doesnt require arrays

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,4 @@
-const sumAll = function(...num) {
+/*const sumAll = function(...num) {
     
      let sum = num.reduce(myFunction);
         function myFunction(total, value, index, array) {
@@ -7,7 +7,19 @@ const sumAll = function(...num) {
             }
         }
 
-};
+};*/
+
+
+function sumAll(a,b) {
+    let sum = 0;
+    for(let i=a; i<=b; i++) {
+        sum=sum+i;
+        }
+        return sum;
+}
+
+sumAll(1,5)
+
 
 // Do not edit below this line
 module.exports = sumAll;
